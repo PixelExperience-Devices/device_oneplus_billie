1af0d7caf7926abcd75c7a645e51655bce35cc7d
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-06 18:20:54 +0300

    billie: disable soundtrigger via vendor overlay
    * for Q devices not using hotword enrollment apks it is necessary to do this if using pixel props
    * this solution is cleaner than overlaying entire manifest.xml
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- device.mk
+++ device.mk
@@ -104,6 +104,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/st_disable.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest/st_disable.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
